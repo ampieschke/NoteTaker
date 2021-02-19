63c625d2800122d5936b958313d0888fe4630deb
Andrew Pieschke <ampieschke@gmail.com>
2021-02-18 21:27:10 -0500

    continueing to work on linking things together

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,15 @@
 // `GET *` should return the `index.html` file.
 
 // `GET /notes` should return the `notes.html` file.
+
+const path = require("path");
+
+module.exports = (app) => {
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
