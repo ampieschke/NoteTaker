ea7beb023e3aad0c494d0ddab710fce2ec066d34
Andrew Pieschke <ampieschke@gmail.com>
2021-02-23 17:57:14 -0500

    updated ports

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const express = require("express");
 const app = express();
 
 const PORT = 8001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
