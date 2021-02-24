71ac19e5025a2d6425392a04002e653ee7e28187
Andrew Pieschke <ampieschke@gmail.com>
2021-02-24 18:14:04 -0500

    updated port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 8001;
+const PORT = process.env.PORT || 8001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
